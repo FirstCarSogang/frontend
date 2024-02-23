edae1fa125da0cf0cf037d7b471b6c167f4cf093
eunsu <songess@naver.com>
2024-02-23 16:45:55 +0900

    slowticket daycard answer제출 로직 구현

--- src/pages/SlowTrain/TicketNumber/NonAnswered/NonAnsweredAfterDay1QCard.tsx
+++ src/pages/SlowTrain/TicketNumber/NonAnswered/NonAnsweredAfterDay1QCard.tsx
@@ -6,8 +6,9 @@ import {
   Stack,
   Text,
   Textarea,
+  useToast,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { type Question, AfterDay1Question } from '@/types/common';
 interface AfterDay1QuestionCardProps {
   question: Question;
@@ -17,6 +18,21 @@ export default function AfterDay1QuestionCard({
   question,
 }: AfterDay1QuestionCardProps) {
   const afterDay1Question = question as AfterDay1Question;
+  const [answer1, setAnswer1] = useState('');
+  const [answer2, setAnswer2] = useState('');//지금 저장되는 값은 치킨
+  const toast = useToast();
+  const submitHandler = () => {
+    if (answer1 === '' || answer2 === '') {
+      toast({
+        title: '답변을 모두 입력해주세요.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+    console.log(answer1, answer2);
+  };
   return (
     <CardFooter p="10px 30px" flex="display" flexDir="column" gap="10px">
       <Text fontSize="16px" as="b">
@@ -27,11 +43,19 @@ export default function AfterDay1QuestionCard({
         mb="10px"
         bg="gray.200"
         focusBorderColor="green.200"
+        value={answer1}
+        onChange={(e) => setAnswer1(e.target.value)}
       />
       <Text fontSize="16px" as="b">
         {afterDay1Question.question2}
       </Text>
-      <RadioGroup colorScheme="green">
+      <RadioGroup
+        colorScheme="green"
+        value={answer2}
+        onChange={(nextValue) => {
+          setAnswer2((prev) => nextValue);
+        }}
+      >
         <Stack direction="column" spacing="5px" mb="10px">
           <Radio value={afterDay1Question.multipleChoiceAnswer1}>
             {afterDay1Question.multipleChoiceAnswer1}
@@ -56,7 +80,7 @@ export default function AfterDay1QuestionCard({
           )}
         </Stack>
       </RadioGroup>
-      <Button colorScheme="green" size="sm" mb="10px">
+      <Button colorScheme="green" size="sm" mb="10px" onClick={submitHandler}>
         제출
       </Button>
     </CardFooter>
--- src/pages/SlowTrain/TicketNumber/NonAnswered/NonAnsweredDay1QCard.tsx
+++ src/pages/SlowTrain/TicketNumber/NonAnswered/NonAnsweredDay1QCard.tsx
@@ -1,13 +1,29 @@
-import { Text, Textarea, CardFooter, Button } from '@chakra-ui/react';
-import React from 'react';
+import { Text, Textarea, CardFooter, Button, useToast } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import { type Question, Day1Question } from '@/types/common';
 
 interface Day1QuestionCardProps {
   question: Question;
 }
 
-export default function NonAnsweredDay1QCard({ question }: Day1QuestionCardProps) {
+export default function NonAnsweredDay1QCard({
+  question,
+}: Day1QuestionCardProps) {
   const day1Question = question as Day1Question;
+  const [answer, setAnswer] = useState('');
+  const toast = useToast();
+  const submitHandler = () => {
+    if (answer === '') {
+      toast({
+        title: '답변을 입력해주세요.',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+    console.log(answer);
+  };
   return (
     <CardFooter p="10px 30px" flex="display" flexDir="column" gap="10px">
       <Text fontSize="16px" as="b">
@@ -18,6 +34,8 @@ export default function NonAnsweredDay1QCard({ question }: Day1QuestionCardProps
         mb="10px"
         bg="gray.200"
         focusBorderColor="green.200"
+        value={answer}
+        onChange={(e) => setAnswer(e.target.value)}
       />
       <Button colorScheme="green" size="sm" mb="10px">
         제출
