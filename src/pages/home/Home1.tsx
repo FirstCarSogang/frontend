981ff323aab5a9a5a4ed55e2d6371ab06788780c
eunsu <songess@naver.com>
2024-02-27 15:20:55 +0900

    refactor: img 위치 변경시도

--- src/pages/home/Home1.tsx
+++ src/pages/home/Home1.tsx
@@ -10,7 +10,7 @@ export default function Home1() {
       h="100%"
       pos="relative"
       flexDir="column"
-      backgroundImage="/img/Train.JPG"
+      backgroundImage="/Train.JPG"
       backgroundSize="cover"
       backgroundPosition="center"
       justify="space-around"
